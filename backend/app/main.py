16730996a67fde88b1cd14064348bd47efa9a4bb
Heitor Vasconcelos <fhv.araujo@gmail.com>
2024-12-17 16:20:49 -0300

    added cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,10 +2,18 @@
 from fastapi import FastAPI
 from .database import Base, engine
 from .routers import wines, moments
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou restrinja ao domínio do frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(wines.router)
 app.include_router(moments.router)
